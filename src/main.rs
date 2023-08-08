0f57e90b75a5a2fdcc90f5d800e1836d017d1bdb
Sebastian Beyvers <sb@pus.de>
2023-08-09 01:18:34 +0200

    feat: First look at clap

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
+use clap::arg;
+use clap::Command;
 use jam_rs::{compare, file_io};
 
-fn main() {
-    let reader = file_io::FileHandler {};
-    let start = std::time::Instant::now();
-    let sketch1 = reader.read_sketches("output.sketch").unwrap();
-    let sketch2 = reader.read_sketches("combined.sketch").unwrap();
+fn cli() -> Command {
+    Command::new("jam")
+        .about("Just another minhasher, obviously blazingly fast")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new("sketch")
+                .about("Create a sketch for file(s)")
+                .arg(arg!(-i --input <FILE> "Input file"))
+                .arg_required_else_help(true)
+                .arg(arg!(-o --output <FILE> "Output file"))
+                .arg_required_else_help(true),
+        )
+        .subcommand(
+            Command::new("diff")
+                .about("Compare two commits")
+                .arg(arg!(base: [COMMIT]))
+                .arg(arg!(head: [COMMIT]))
+                .arg(arg!(path: [PATH]).last(true))
+                .arg(
+                    arg!(--color <WHEN>)
+                        .value_parser(["always", "auto", "never"])
+                        .num_args(0..=1)
+                        .require_equals(true)
+                        .default_value("auto")
+                        .default_missing_value("always"),
+                ),
+        )
+        .arg(arg!(--version).exclusive(true))
+        .arg(arg!(--threads -t <THREADS>).global(true))
+}
 
-    let mut comparator = compare::MultiComp::new(sketch1, sketch2);
-    comparator.compare();
-    for x in comparator.finalize().iter() {
-        println!("{}", x);
+fn main() {
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(a) => (),
+        None => (),
     }
-    let elapsed = start.elapsed().as_millis();
-    println!("Processed in {:?} seconds", elapsed as f64 / 1000.0);
 }
